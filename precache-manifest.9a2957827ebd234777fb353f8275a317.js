f35ed925b701cd955ed1c4daa6fafc51211b6d17
kmmadu <mmadubugwukingsley@gmail.com>
2024-06-06 10:51:17 +0100

    Updates

---
+++ precache-manifest.9a2957827ebd234777fb353f8275a317.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88f7975f3bf6971c6ba759e01df8751a",
+    "url": "/App_Task_Master/index.html"
+  },
+  {
+    "revision": "88654c0e69954e323a99",
+    "url": "/App_Task_Master/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "7d6b9f8978281162c50d",
+    "url": "/App_Task_Master/static/js/2.fd5371a8.chunk.js"
+  },
+  {
+    "revision": "fbdf06f93aee7a6140e2",
+    "url": "/App_Task_Master/static/js/3.c8abb739.chunk.js"
+  },
+  {
+    "revision": "88654c0e69954e323a99",
+    "url": "/App_Task_Master/static/js/main.26948cee.chunk.js"
+  },
+  {
+    "revision": "1d85c10e51be6f1b4bf1",
+    "url": "/App_Task_Master/static/js/runtime~main.e1750512.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/App_Task_Master/static/media/logo.06e73328.svg"
+  }
+]);
